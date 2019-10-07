c96bd9fccb0ffb03ba39698da9e5c500f85ac1d3
george <glagakis@gmail.com>
2019-10-07 07:49:15 +0300

    εβαλα στα trapezia recyclerView

--- app/src/main/java/com/example/ioun25/trapezia.java
+++ app/src/main/java/com/example/ioun25/trapezia.java
@@ -1,21 +1,35 @@
 package com.example.ioun25;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteAccessPermException;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
+
+
+
 
 import static com.example.ioun25.MainActivity.EXTRA_MESSAGE;
 
@@ -25,9 +39,53 @@ public class trapezia extends AppCompatActivity {
     ArrayList<String> pel;
     ArrayList<String> EIDH;
     GridView moviesList;
+
     ArrayList<String> KATHG;
     public String[] arrIdParagg=new String[100];
 
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
+    private ArrayList<String> planetList=new ArrayList();
+//Other Stuff
+
+    private final String[] mPlanets = new String[] {
+            "Mercury",
+            "Venus",
+            "Earth",
+            "Mars",
+            "Jupiter",
+            "Saturn",
+            "Uranus",
+            "Neptune"
+    };
+
+
+
+    private class PlanetViewHolder
+            extends RecyclerView.ViewHolder
+            implements View.OnClickListener {
+
+        public PlanetViewHolder(View v) {
+            super(v);
+            v.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(getApplicationContext(),
+                    "You have clicked " + ((TextView) v).getText(),
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+
+
+
+
+  //  MyRecyclerViewAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,6 +104,38 @@ public class trapezia extends AppCompatActivity {
         // Capture the layout's TextView and set the string as its text
         TextView textView = findViewById(R.id.textView);
         textView.setText(message);
+
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.grid2);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setAdapter(new RecyclerView.Adapter<PlanetViewHolder>() {
+
+            @Override
+            public PlanetViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                View v = LayoutInflater.from(parent.getContext()).inflate(
+                        android.R.layout.simple_list_item_1,
+                        parent,
+                        false);
+                PlanetViewHolder vh = new PlanetViewHolder(v);
+                return vh;
+            }
+
+            @Override
+            public void onBindViewHolder(PlanetViewHolder vh, int position) {
+                TextView tv = (TextView) vh.itemView;
+                tv.setText(mPlanets[position]);
+                tv.setCompoundDrawablePadding(24);
+                tv.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_stars_black_24dp, 0, 0, 0);
+            }
+
+            @Override
+            public int getItemCount() {
+                return mPlanets.length;
+            }
+        });
+
+
+
 
 
 
@@ -98,6 +188,14 @@ public class trapezia extends AppCompatActivity {
 
         listTRAPEZIA();
 
+
+
+
+
+
+
+
+
     }
 
 
@@ -108,7 +206,8 @@ public class trapezia extends AppCompatActivity {
         SQLiteDatabase mydatabase=null;
         Integer n=0;
         moviesList=(GridView)findViewById(R.id.grid);
-        List<String> values=new ArrayList<>();
+        //recyclerView=(RecyclerView) findViewById(R.id.grid2);
+        final List<String> values=new ArrayList<>();
 
         try{
             mydatabase = openOrCreateDatabase("eidh",MODE_PRIVATE,null);
@@ -128,14 +227,38 @@ public class trapezia extends AppCompatActivity {
             mydatabase.close();
             ArrayAdapter<String> arrayAdapter =
                     new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1, values);
-
             moviesList.setAdapter(arrayAdapter);
+
+           // TrapeziaList.setAdapter(arrayAdapter);
+
+            //final MyAdapter adapter = new MyAdapter();
+          //  rv.setAdapter(adapter);
+         //   GridLayoutManager mLayoutManager = new GridLayoutManager(this, 2);
+          //  rv.setLayoutManager(mLayoutManager);
+
+// set up the RecyclerView
+         //   RecyclerView recyclerView = findViewById(R.id.rvAnimals);
+        //    TrapeziaList.setLayoutManager(new LinearLayoutManager(this));
+         //   adapter = new MyRecyclerViewAdapter(this, animalNames);
+         //   adapter.setClickListener(this);
+         //   recyclerView.setAdapter(adapter);
+
+
+
+
+
+
+
+
+
+
 
 
         } catch (SQLiteAccessPermException e) {
             e.printStackTrace();
         }
     }
+
 
 
 
