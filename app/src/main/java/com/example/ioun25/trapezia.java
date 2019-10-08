85565a1daeb4450eb5a888a2590c27bf4f2b4f92
george <glagakis@gmail.com>
2019-10-08 17:55:00 +0300

    στο τραπεζια επιλεγω τραπαζι και μετα πατω το πληκτρο PARAGGELIA

--- app/src/main/java/com/example/ioun25/trapezia.java
+++ app/src/main/java/com/example/ioun25/trapezia.java
@@ -1,17 +1,27 @@
 package com.example.ioun25;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+
+
+
 
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteAccessPermException;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.Rect;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -25,12 +35,16 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+// import com.dgreenhalgh.android.simpleitemdecoration.grid.GridDividerItemDecoration;
+
+import com.dgreenhalgh.android.simpleitemdecoration.grid.GridDividerItemDecoration;
+import com.dgreenhalgh.android.simpleitemdecoration.grid.GridTopOffsetItemDecoration;
+
 import java.util.ArrayList;
 import java.util.List;
 
 
-
-
+import static androidx.recyclerview.widget.DividerItemDecoration.VERTICAL;
 import static com.example.ioun25.MainActivity.EXTRA_MESSAGE;
 
 //  import static com.example.ioun25.MainActivity.pel;
@@ -39,6 +53,9 @@ public class trapezia extends AppCompatActivity {
     ArrayList<String> pel;
     ArrayList<String> EIDH;
     GridView moviesList;
+
+    TextView trapezi;
+
 
     ArrayList<String> KATHG;
     public String[] arrIdParagg=new String[100];
@@ -51,15 +68,42 @@ public class trapezia extends AppCompatActivity {
 //Other Stuff
 
     private final String[] mPlanets = new String[] {
-            "Mercury",
-            "Venus",
-            "Earth",
-            "Mars",
-            "Jupiter",
-            "Saturn",
-            "Uranus",
-            "Neptune"
+            "Mercury aaaa",
+            "Venus dfd",
+            "Earth fgf",
+            "Mars fgfg",
+            "Jupiter fgf",
+            "Saturn fgf",
+            "Uranus gf",
+            "Neptune gfg fgf"
     };
+
+
+
+
+  /*
+    GridLayoutManager gridLayoutManager = new GridLayoutManager(this, spanCount);
+
+gridLayoutManager.setSpanSizeLookup(new GridLayoutManager.SpanSizeLookup() {
+        @Override
+        public int getSpanSize(int position) {
+            //define span size for this position
+            //for example, if you have 2 column per row, you can implement something like that:
+            if(position == youRule) {
+                return 2; //item will take 2 column (full row size)
+            } else {
+                return 1; //you will have 2 rolumn per row
+            }
+        }
+    });
+*/
+
+
+
+
+
+
+
 
 
 
@@ -102,12 +146,65 @@ public class trapezia extends AppCompatActivity {
         KATHG = intent.getStringArrayListExtra("mKathg");
 
         // Capture the layout's TextView and set the string as its text
-        TextView textView = findViewById(R.id.textView);
-        textView.setText(message);
+     //   TextView textView = findViewById(R.id.textView);
+      //  textView.setText(message);
 
 
         mRecyclerView = (RecyclerView) findViewById(R.id.grid2);
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+      //  mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setLayoutManager(new GridLayoutManager(this,3));
+
+
+
+/*  κανει τα ιδια με την βιβλιοθηκη 'com.bignerdranch.android:simple-item-decoration:1.0.0'
+// αλλα μπορει να γινει null και με βγαζει προειδοποιησεις το Android Stud
+        DividerItemDecoration Hdivider = new DividerItemDecoration(mRecyclerView.getContext(), DividerItemDecoration.HORIZONTAL);
+        DividerItemDecoration Vdivider = new DividerItemDecoration(mRecyclerView.getContext(), DividerItemDecoration.VERTICAL);
+        Hdivider.setDrawable(ContextCompat.getDrawable(getBaseContext(), R.drawable.line_divider));
+        Vdivider.setDrawable(ContextCompat.getDrawable(getBaseContext(), R.drawable.line_divider));
+        mRecyclerView.addItemDecoration(Hdivider);
+        mRecyclerView.addItemDecoration(Vdivider);
+*/
+
+
+
+
+
+
+
+
+
+    //    private GridLayoutManager mGridLayoutManager;
+// ...
+    //    mGridLayoutManager = new GridLayoutManager(getActivity(), 2);
+
+      //  int spacingInPixels = getResources().getDimensionPixelSize(R.dimen.spacing);
+      //  mRecyclerView.addItemDecoration(new SpacesItemDecoration(spacingInPixels));
+
+
+        /* δουλευει με οριζοντιεσ γραμμεσ
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(mRecyclerView.getContext(),VERTICAL); // HORIZONTAL or VERTICAL.
+        mRecyclerView.addItemDecoration(dividerItemDecoration);
+         */
+
+
+
+        // με την βιβλιοθηκη 'com.bignerdranch.android:simple-item-decoration:1.0.0'
+        int numColumns=3;
+        Drawable horizontalDivider = ContextCompat.getDrawable(this, R.drawable.line_divider);
+        Drawable verticalDivider = ContextCompat.getDrawable(this, R.drawable.line_divider);
+
+        mRecyclerView.addItemDecoration(new GridDividerItemDecoration(horizontalDivider, verticalDivider, numColumns));
+        int offsetPx = 10;
+        mRecyclerView.addItemDecoration(new GridTopOffsetItemDecoration(offsetPx, numColumns));
+
+
+
+
+
+
+
+
         mRecyclerView.setAdapter(new RecyclerView.Adapter<PlanetViewHolder>() {
 
             @Override
@@ -124,8 +221,17 @@ public class trapezia extends AppCompatActivity {
             public void onBindViewHolder(PlanetViewHolder vh, int position) {
                 TextView tv = (TextView) vh.itemView;
                 tv.setText(mPlanets[position]);
-                tv.setCompoundDrawablePadding(24);
-                tv.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_stars_black_24dp, 0, 0, 0);
+                tv.setTextSize(TypedValue.COMPLEX_UNIT_SP, 10f);
+                // αν θελω εικονα
+              //  tv.setCompoundDrawablePadding(24);
+              //  tv.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_stars_black_24dp, 0, 0, 0);
+                // αν θελω εικονα
+
+
+
+
+
+
             }
 
             @Override
@@ -173,6 +279,14 @@ public class trapezia extends AppCompatActivity {
                                     long id) {
 
                 Object o = moviesList.getItemAtPosition(position);
+                trapezi = (TextView)findViewById(R.id.textView);
+                //  textView.setText(message);
+
+                trapezi.setText(o.toString()+";"+arrIdParagg[position]);
+
+
+
+       /*   αυτο ολο θα μεταφερθεί στο κουμπι της παραγγελίας
 
 
                 Intent intent = new Intent(view.getContext(), order.class);
@@ -183,22 +297,36 @@ public class trapezia extends AppCompatActivity {
                 intent.putExtra("mKATHG", KATHG); // ΣΤΕΛΝΩ ΤΟΝ ΠΙΝΑΚΑ ΜΕ ΤΑ EIDH
                 // intent.putExtra("mpel", pel); // ΣΤΕΛΝΩ ΤΟΝ ΠΙΝΑΚΑ ΜΕ ΤΑ ΤΡΑΠΕΖΙΑ
                 trapezia.this.startActivity(intent);
+
+       */
+
+
+
+
+
             }
         });
 
         listTRAPEZIA();
-
-
-
-
-
-
-
-
-
     }
 
 
+  public void Paraggelia(View view) {
+      Intent intent = new Intent(view.getContext(), order.class);
+      // intent.putExtra(EXTRA_MESSAGE, o.toString());
+      String ctrapezi;
+      trapezi = (TextView)findViewById(R.id.textView);
+      ctrapezi=trapezi.getText().toString();
+
+      intent.putExtra("mpel2", ctrapezi);  // αριθμος τραπεζιου
+      intent.putExtra("mpel", pel); // ΣΤΕΛΝΩ ΤΟΝ ΠΙΝΑΚΑ ΜΕ ΤΑ ΤΡΑΠΕΖΙΑ
+      intent.putExtra("mEIDH", EIDH); // ΣΤΕΛΝΩ ΤΟΝ ΠΙΝΑΚΑ ΜΕ ΤΑ EIDH
+      intent.putExtra("mKATHG", KATHG); // ΣΤΕΛΝΩ ΤΟΝ ΠΙΝΑΚΑ ΜΕ ΤΑ EIDH
+      // intent.putExtra("mpel", pel); // ΣΤΕΛΝΩ ΤΟΝ ΠΙΝΑΚΑ ΜΕ ΤΑ ΤΡΑΠΕΖΙΑ
+      trapezia.this.startActivity(intent);
+
+
+  }
 
 
     //================= sqlliteEIDH====================
@@ -258,6 +386,7 @@ public class trapezia extends AppCompatActivity {
             e.printStackTrace();
         }
     }
+
 
 
 
