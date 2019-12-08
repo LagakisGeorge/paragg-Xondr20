c02da17c69092c71ef20ce77a042bcc63649eacb
User <glagakis@gmail.com>
2019-12-08 23:38:10 +0200

    KATHGORIES intent

---
+++ app/src/main/java/com/example/ioun25/GENERALPARAMETERS.java
@@ -0,0 +1,14 @@
+package com.example.ioun25;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GENERALPARAMETERS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_generalparameters);
+    }
+}
